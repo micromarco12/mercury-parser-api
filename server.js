883c7e75d9487346c4a21bbf2ff6c906a6011999
micromarco12 <marco@ciirus.com>
2025-04-21 12:47:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get('/parser', async (req, res) => {
 // ✅ Updated here: use process.env.PORT for Render compatibility
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
